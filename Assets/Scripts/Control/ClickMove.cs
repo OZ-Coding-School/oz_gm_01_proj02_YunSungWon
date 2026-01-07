33806f7f4e0a47ab7412333bdd90c65d52df7b30
ghost-backseat-driver <ysl2323@naver.com>
2026-01-07 13:39:15 +0900

    feat : 오브젝트 클릭→자동이동→자동상호작용→결과반응 이어지는 UX 완성

--- Assets/Scripts/Control/ClickMove.cs
+++ Assets/Scripts/Control/ClickMove.cs
@@ -5,7 +5,10 @@ using UnityEngine.AI;
 
 
 /// <summary>
-/// 네비메쉬 클릭이동 테스트 급하게 일단 움직이는거만,
+/// 마우스 클릭기반 위주
+/// -바닥 클릭->위치로 이동
+/// -상호작용 오브젝트 클릭->해당 오브젝트의 상호작용포인트로 이동 + 타겟 저장
+/// 상호작용 실행 자체는 '도착후'를 기점으로 처리
 /// </summary>
 public class ClickMove : MonoBehaviour
 {
@@ -15,7 +18,19 @@ public class ClickMove : MonoBehaviour
     [Header("레이 거리")]
     [SerializeField] private float rayDistance = 100.0f;
 
+    [Header("바닥 레이어")]
+    [SerializeField] private LayerMask groundMask;
+
+    [Header("상호작용 레이어")]
+    [SerializeField] private LayerMask interactableMask;
+
     private NavMeshAgent agent;
+
+    //상호작용할 타겟 저장용
+    private InteractTarget curTarget;
+
+    //저장된 타겟 외부접근용
+    public InteractTarget CurTarget { get { return curTarget; } }
 
     private void Awake()
     {
@@ -30,14 +45,63 @@ public class ClickMove : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            bool isHit = Physics.Raycast(ray, out hit, rayDistance);
+            ClickHandle();
+        }
+    }
 
-            if (isHit)
+    /// <summary>
+    /// 마우스 클릭 피드백 관리
+    /// </summary>
+    private void ClickHandle()
+    {
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        //상호작용 오브젝트 우선적으로 판별하고 이동
+        bool hitInteractable = Physics.Raycast(ray, out hit, rayDistance, interactableMask);
+        if (hitInteractable)
+        {
+            InteractTarget target = hit.collider.GetComponentInParent<InteractTarget>();
+            if (target != null && target.Interactable != null)
             {
-                agent.SetDestination(hit.point);
+                SetTargetAndMove(target);
+                return;
             }
         }
+        //바닥 클릭하면 저장타겟을 클리어, 이동만
+        bool hitGround = Physics.Raycast(ray, out hit, rayDistance, groundMask);
+        if (hitGround)
+        {
+            ClearTarget();
+            MoveTO(hit.point);
+        }
+    }
+
+    //상호작용 타겟지정+이동
+    private void SetTargetAndMove(InteractTarget target)
+    {
+        curTarget = target;
+        Vector3 destination = target.InteractPoint.position;
+        MoveTO(destination);
+
+        Debug.Log("[ClickMove] 타겟 지정됨,여기로 이동할게?" + target.name);
+    }
+
+    /// <summary>
+    /// 목적지로 이동기능만
+    /// </summary>
+    /// <param name="destination"></param>
+    private void MoveTO(Vector3 destination)
+    {
+        agent.isStopped = false;
+        agent.SetDestination(destination);
+    }
+
+    /// <summary>
+    /// 저장 타겟 초기화
+    /// </summary>
+    public void ClearTarget()
+    {
+        curTarget = null;
     }
 }
