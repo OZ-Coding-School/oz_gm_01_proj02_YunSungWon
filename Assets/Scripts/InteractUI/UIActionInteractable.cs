c5b9aa35e63b71f975b2cbece971a2824f6ecdea
ghost-backseat-driver <ysl2323@naver.com>
2026-01-13 17:07:02 +0900

    fix : 액션형 상호작용UI, 상태저장을 패널에 했던 이슈,-> 리셋테이블이 계속 초기화, 상태를 월드 오브젝트가 들고있게 리팩토링

---
+++ Assets/Scripts/InteractUI/UIActionInteractable.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 액션형 UI 상호작용
+/// ->클릭-이동-도착-UI오픈 후
+/// 실제 효과는 UI버튼 클릭 시점에 적용되게 변경
+/// 
+/// 약통, 핸드폰 UI와 관련
+/// </summary>
+public class UIActionInteractable : InteractableBase
+{
+    [Header("열 UI 패널(UI액션패널베이스)")]
+    [SerializeField] private UIActionPanelBase panel;
+
+    [Header("디버그/연출용 사유")]
+    [SerializeField] private string reason = "UIAction 상호작용";
+
+    public override void Interact(GameObject interactor)
+    {
+        if (UIManager.Instance == null)
+        {
+            Debug.Log("[UIViewInteractable] UIManager.Instance 가 null 났음");
+            return;
+        }
+
+        InteractContext context = new InteractContext(interactor, gameObject, reason);
+        UIManager.Instance.OpenPanel(panel, context);
+    }
+}
